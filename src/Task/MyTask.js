adeaaacc070ee2c79b71708c2a74fb5ad36a4fea
Sandeep Sharma <sandeepsharma10697@gmail.com>
2023-01-08 12:53:44 +0530

    making faltu changes

--- src/Task/MyTask.js
+++ src/Task/MyTask.js
@@ -23,7 +23,6 @@ export default function MyTask() {
       .then((res) => res.json())
       .then((data) => {
         setTasks(data.tasks);
-        // window.location.reload();
       });
   };
 
